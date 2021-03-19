20252cf0728aeed2253b21e87d7cf8f3f3a2f3b6
Freddy Marroquin <fl.mlopezgt@gmail.com>
2021-03-18 18:47:31 -0600

    *Dashboard, deleted template examples

--- resources/views/includes/panel/dropdown_menu.blade.php
+++ resources/views/includes/panel/dropdown_menu.blade.php
@@ -2,7 +2,7 @@
     <div class=" dropdown-header noti-title">
         <h6 class="text-overflow m-0">Bienvenido!</h6>
     </div>
-    <a href="./examples/profile.html" class="dropdown-item">
+    <!-- <a href="./examples/profile.html" class="dropdown-item">
         <i class="ni ni-single-02"></i>
         <span>Mi Perfil</span>
     </a>
@@ -17,7 +17,7 @@
     <a href="./examples/profile.html" class="dropdown-item">
         <i class="ni ni-support-16"></i>
         <span>Ayuda</span>
-    </a>
+    </a> -->
     <div class="dropdown-divider"></div>
     <a href="{{route('logout')}}" class="dropdown-item" onclick="event.preventDefault(); document.getElementById('logout3').submit();" >
         <i class="ni ni-user-run"></i>
--- resources/views/includes/panel/menu/doctor.blade.php
+++ resources/views/includes/panel/menu/doctor.blade.php
@@ -8,8 +8,8 @@
             <i class="ni ni-calendar-grid-58 text-danger"></i> Mis Citas
         </a>
     </li>
-<li class="nav-item">
+<!-- <li class="nav-item">
     <a class="nav-link" href="{{ url('/specialties') }}">
         <i class="ni ni-planet text-blue"></i> Mis Pacientes
     </a>
-</li>
+</li> -->
